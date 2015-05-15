4d75c2baca8e438b9961d21495e3d75023bebb24
mubi <msadoon@gmail.com>
2015-05-15 00:21:51 -0400

    added keyboard methods to dismiss keyboard

--- JomJom/JomJom/CreatePostViewController.swift
+++ JomJom/JomJom/CreatePostViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CreatePostViewController: UIViewController, UITextFieldDelegate {
+class CreatePostViewController: UIViewController, UITextFieldDelegate, UIGestureRecognizerDelegate {
 
     var utilityInstance:Utilities = Utilities.utilitiesInstance
     var buttonWidth:CGFloat?
@@ -25,6 +25,7 @@ class CreatePostViewController: UIViewController, UITextFieldDelegate {
         setupPostText()
         setupScrollView()
         setupKeyboardListeners()
+        setupGestureRecognizers()
     }
     
     override func didReceiveMemoryWarning() {
@@ -82,14 +83,6 @@ class CreatePostViewController: UIViewController, UITextFieldDelegate {
         self.navigationController?.popViewControllerAnimated(true)
     }
     
-    func setupPostSpace() {
-        var navbarHeight = self.navigationController?.navigationBar.bounds.height as CGFloat!
-        heightOfPostSpace = CGFloat(navbarHeight+20)
-        postView = UIView(frame: CGRectMake(0, 0, CGFloat(utilityInstance.getScreenWidth()), CGFloat(utilityInstance.getScreenHeight())-heightOfPostSpace!-44))
-        var randomColorNumber = utilityInstance.rollRandomColor()
-        postView!.backgroundColor = pickRandomColor(randomColorNumber)
-    }
-    
     func pickRandomColor(num:Int) -> UIColor{
         var applyColor:UIColor
         switch (num) {
@@ -113,6 +106,14 @@ class CreatePostViewController: UIViewController, UITextFieldDelegate {
         return applyColor
     }
     
+    func setupPostSpace() {
+        var navbarHeight = self.navigationController?.navigationBar.bounds.height as CGFloat!
+        heightOfPostSpace = CGFloat(navbarHeight+20)
+        postView = UIView(frame: CGRectMake(0, 0, CGFloat(utilityInstance.getScreenWidth()), CGFloat(utilityInstance.getScreenHeight())-heightOfPostSpace!-44))
+        var randomColorNumber = utilityInstance.rollRandomColor()
+        postView!.backgroundColor = pickRandomColor(randomColorNumber)
+    }
+    
     func setupPostText() {
         textView = UIView(frame: CGRectMake(0, postView!.frame.height, CGFloat(utilityInstance.getScreenWidth()), 44))
         textView!.backgroundColor = utilityInstance.UIColorFromRGB(0xCBD6DD)
@@ -120,6 +121,7 @@ class CreatePostViewController: UIViewController, UITextFieldDelegate {
         var textBox:UITextField = UITextField(frame: CGRectMake(15, 9.5, CGFloat(utilityInstance.getScreenWidth()/1.30), 25))
         textBox.borderStyle = UITextBorderStyle.RoundedRect
         textBox.backgroundColor = UIColor.whiteColor()
+        textBox.delegate = self
         var prefSendButtonWidth:CGFloat = CGFloat(utilityInstance.getScreenWidth()/10)
         var prefSendButtonHeight:CGFloat = prefSendButtonWidth*0.85185
         var sendButton:UIButton = UIButton(frame: CGRectMake(textBox.frame.width+32, 9.5, prefSendButtonHeight, 25))
@@ -133,15 +135,22 @@ class CreatePostViewController: UIViewController, UITextFieldDelegate {
     
     func setupScrollView() {
         scrollView = UIScrollView(frame: CGRectMake(0, 0, CGFloat(utilityInstance.getScreenWidth()), CGFloat(utilityInstance.getScreenHeight())-heightOfPostSpace!))
+        scrollView!.contentSize = CGSizeMake(CGFloat(utilityInstance.getScreenWidth()), CGFloat(utilityInstance.getScreenHeight())-heightOfPostSpace!)
         scrollView?.addSubview(postView!)
         scrollView?.addSubview(textView!)
-        scrollView!.contentSize = CGSizeMake(CGFloat(utilityInstance.getScreenWidth()), CGFloat(utilityInstance.getScreenHeight())-heightOfPostSpace!)
+        
         self.view.addSubview(scrollView!)
     }
     
     func setupKeyboardListeners() {
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func setupGestureRecognizers() {
+        var dismissKeyboard:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "keyboardDismissTap:")
+        dismissKeyboard.delegate = self
+        self.view.addGestureRecognizer(dismissKeyboard)
     }
     
     //MARK: Keyboard methods
@@ -160,6 +169,13 @@ class CreatePostViewController: UIViewController, UITextFieldDelegate {
 //        scrollView! = CGRectMake(scrollView!.frame.origin.x, scrollView!.frame.origin.y, scrollView!.frame.width, scrollView!.frame.height-keyboardFrame.height)
     }
     
+    //MARK: Gesture Recognizer Methods
+    
+    func keyboardDismissTap(gesture: UITapGestureRecognizer) {
+        println(gesture)
+        self.view.endEditing(true)
+    }
+    
     //MARK: TextField Delegate Methods
     
     func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
@@ -170,7 +186,6 @@ class CreatePostViewController: UIViewController, UITextFieldDelegate {
     
     func textFieldShouldEndEditing(textField: UITextField) -> Bool {
         //resize view
-        
         return true
     }
     
